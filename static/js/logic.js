ca319588dea6a685b2b86c60f08c4d87ec103f45
Meeyoung Park <parkmee@gmail.com>
2023-01-03 21:26:35 -0500

    1st dropdown - WIP

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,21 @@
 // code for drop down menu
+var transit_data = d3.csv('data/transit_rail_station.csv').then(d => d);
+
+// stations = []
+
+function init() {
+
+    transit_data.then(function(d){
+        d.forEach(item => {
+            d3.select('#martaStation')
+                .append('option')
+                .text(item.station)
+                .property('value', item.station)
+        })
+    })
+}
+
+// init()
 
 // code for plots below
 
