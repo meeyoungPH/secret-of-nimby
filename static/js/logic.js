5e563961c714c02184a1e9520261d9727fbd9935
Meeyoung Park <parkmee@gmail.com>
2023-01-03 23:13:29 -0500

    populated dropdown menus WIP

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,22 +1,82 @@
-// code for drop down menu
-var transit_data = d3.csv('data/transit_rail_station.csv').then(d => d);
+// save data to variables
+var crime_data = d3.csv('data/cobra_summary.csv').then(d => d);
+var neighborhood_data = d3.json('data/City_of_Atlanta_Neighborhood_Statistical_Areas.geojson').then(d => d);
+var station_data = d3.csv('data/transit_rail_station.csv').then(d => d);
 
-// stations = []
-
-function init() {
-
-    transit_data.then(function(d){
-        d.forEach(item => {
-            d3.select('#martaStation')
-                .append('option')
-                .text(item.station)
-                .property('value', item.station)
-        })
-    })
+// initialize webpage
+function init(){
+    populateDropdowns()
 }
 
-init()
+// TODO: select dropdown menu format that displays the selected value
+// TODO: sort values in dropdown menus - below does not work
+// var sorted_stations = station_data.sort((a,b) => d3.ascending(a.station, b.station))
+
+// populate dropdown menus
+function populateDropdowns() {
+    
+    // neighborhood dropdown menu
+    // TODO: create common neighborhood name columns in each table
+    neighborhood_data.then((d) => {
+        let results = d.features
+        console.log(results);
+
+        // create dropdown elements
+        results.forEach(row => {
+            d3.select('#neighborhood')
+                .append('option')
+                .text(row.properties.A)
+                .property('value', row.properties.A)
+        });
+    });
+
+    // crime dropdown menu
+    crime_data.then((data) => {
+        console.log(data);
+
+        // capture unique crime_type values
+        let options = [...new Set(data.map(d => d.crime_type))];
+
+        // create dropdown elements
+        d3.select('#crimeType')
+            .selectAll('option')
+                .data(options)
+            .enter()
+                .append('option')
+                .text(d => d)
+                .attr('value', d => d);
+    });
+
+    // station dropdown menu
+    station_data.then((d) => {
+        console.log(d);
+
+        // create dropdown elements
+        d.forEach(row => {
+            d3.select('#martaStation')
+                .append('option')
+                .text(row.station)
+                .property('value', row.station)
+        });
+    });
+};
+
+// FUNCTIONS TODO - change in HTML
+function neighborhoodChanged(value){
+
+}
+
+function crimeChanged(value){
+
+}
+
+function stationChanged(value){
+
+}
 
 // code for plots below
 
 // code for map below
+
+init()
+
