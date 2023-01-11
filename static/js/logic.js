72c4149d08801a5ccb12f6b53644cccd4342f67d
Meeyoung Park <parkmee@gmail.com>
2023-01-10 20:15:18 -0500

    added layers to map

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,6 +14,8 @@ var station_data = d3.json(station_path).then(d => d);
 function init(){
     populateDropdowns()
     stationPoints()
+    neighborhoodBoundaries()
+    // crimePoints("ASSAULT")
 }
 
 // TODO: select dropdown menu format that displays the selected value
@@ -25,10 +27,10 @@ function populateDropdowns() {
     // neighborhood dropdown menu
     neighborhood_data.then((d) => {
         let results = d.features
-        console.log(results);
+        // console.log(results);
 
-        // sort neighborhood names
-        let options = [...new Set(results.map(d => d.properties.A))];
+        // save neighborhood names and code to array and sort
+        let options = [...new Set(results.map(d => [d.properties.A, d.properties.STATISTICA]))];
         options.sort(d3.ascending);
 
         // create dropdown elements
@@ -37,15 +39,15 @@ function populateDropdowns() {
                 .data(options)
             .enter()
                 .append('option')
-                .text(d => d)
+                .text(d => d[0])
                 .attr('class','dd_options')
-                .attr('value', d => d);
+                .attr('value', d => d[1]);
     });
 
     // crime dropdown menu
     crime_data.then((d) => {
         let results = d.features;
-        console.log(results);
+        // console.log(results);
 
         // capture unique crime_type values
         let options = [...new Set(results.map(d => d.properties.crime_type))];
@@ -67,16 +69,21 @@ function populateDropdowns() {
     // station dropdown menu
     station_data.then((d) => {
         let results = d.features;
-        // console.log(results);
+        console.log(results);
+
+        // save station name and neighborhood code to array and sort
+        let options = [...new Set(results.map(d => d.properties.STATION))];
+        options.sort(d3.ascending);
 
         // create dropdown elements
-        results.forEach(row => {
-            d3.select('#martaStation')
+        d3.select('#martaStation')
+            .selectAll('option')
+                .data(options)
+            .enter()
                 .append('option')
-                .text(row.properties.STATION)
+                .text(d => d)
                 .attr('class','dd_options')
-                .property('value', row.properties.STATION)
-        });
+                .attr('value', d => d);
     });
 };
 
@@ -131,8 +138,9 @@ var overlayMaps = {
 };
 
 var myMap = L.map("myMap", {
-    center: [33.76299464274702, -84.42307142385204],
-    zoom: 11.5,
+    // center: [33.76299464274702, -84.42307142385204],
+    center: [33.80642799242456, -84.41307142385204],
+    zoom: 11,
     layers: [street, neighborhoodLayer, stationLayer]
 });
 
@@ -143,7 +151,7 @@ L.control.layers(baseMap, overlayMaps, {
 function stationPoints(station) {
     d3.json(station_path).then(d => {
         let results = d.features
-        console.log(results);
+        // console.log(results);
         
         // function to population popup foe each feature
         function onEachFeature(feature, layer) {
@@ -156,15 +164,39 @@ function stationPoints(station) {
         L.geoJSON(d, {
             onEachFeature: onEachFeature
         }).addTo(stationLayer);
-
-        // show layer on load
-        stationLayer.addTo(myMap);
     })
 }
 
 function neighborhoodBoundaries(neighborhood) {
+    d3.json(neighborhood_path).then(d => {
+        L.geoJSON(d, {
+            color: "darkgreen",
+            opacity: 1,
+            weight: 2
+        }).addTo(neighborhoodLayer);
+    })
+}
 
+function crimePoints(crimeType){
+    // d3.json(crime_path).then(d => {
+    //     console.log(d.features)
 
+    //     // function to population popup for each feature
+    //     function onEachFeature(feature, layer) {
+    //         let results = feature.properties;
+
+    //         layer.bindPopup(
+    //             `<h3>${results.crime_type}</h3>
+    //             <hr>
+    //             <p>Closet MARTA rail station: ${results.closest_station}</p>
+    //             <p>Distance away from closest MARTA rail station: ${results.distance_away} km</p>
+    //             <p>latitude: ${feature.geometry.coordinates[0].toFixed(6)}, longitude: ${feature.geometry.coordinates[1].toFixed(6)}</p>`);
+    //     };
+
+    //     L.geoJSON(d, {
+    //         onEachFeature: onEachFeature
+    //     }).addTo(crimeLayer);
+    // })
 }
 
 
