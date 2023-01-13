c5686d88b72172392a75c34c8aa3a0f36a20325e
annalysebergman <annalysebergman@gmail.com>
2023-01-12 20:30:14 -0600

    bar chart code updated

--- static/js/logic.js
+++ static/js/logic.js
@@ -100,13 +100,20 @@ function stationChanged(value){
 // code for plots below
 
 //Bar chart
-function createBarChart(neighborhood, crimeType) {
+function createBarChart(crimeType) {
     crime_data.then((data) => {
 
         d3.json(crime_path).then(d => {
             let results = d.features;
 
-        console.log(barchart);
+            let xArray = ['AGG ASSAULT', 'AUTO THEFT', 'BURGLARY', 'HOMICIDE', 'LARCENY-FROM VEHICLE', 'LARCNEY-NON VEHICLE','ROBBERY'];
+
+            let yArray = [];
+
+        array = [...new Set(results.filter(d => d.properties.neighborhood == neighborhood))]
+            xArray = [...new Set(array.map(d => [d.properties.lat, d.properties.long]))];
+        
+            //for loop through crimes 
         
         let trace1= {
             x:data(row => row.crime),
