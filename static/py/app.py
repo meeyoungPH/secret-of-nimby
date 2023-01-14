2355bf249350b8fc6dee5be5132ac3940f7b37d9
Meeyoung Park <parkmee@gmail.com>
2023-01-14 10:02:07 -0500

    route for bar chart

--- static/py/app.py
+++ static/py/app.py
@@ -1,10 +1,10 @@
 # dependencies
-from flask import Flask, jsonify
+from flask import Flask
 import pandas as pd
-import datetime as dt
+# import datetime as dt
 import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+# from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from config import username, password
 import numpy
@@ -19,15 +19,15 @@ rds_connection_string = f'{protocol}://{username}:{password}@{host}:{port}/{data
 engine = create_engine(rds_connection_string)
 
 # reflect database into model
-Base = automap_base()
+# Base = automap_base()
 
 # reflect tables
-Base.prepare(engine, reflect=True)
+# Base.prepare(engine, reflect=True)
 
 # save references to tables
-Crime = Base.classes.cobra_merged
-Marta = Base.classes.transit_rail_station
-Neighborhood = Base.classes.neighborhood_data
+# Crime = Base.classes.cobra_merged
+# Marta = Base.classes.transit_rail_station
+# Neighborhood = Base.classes.neighborhood_data
 
 # create app
 app = Flask(__name__)
@@ -39,8 +39,7 @@ def home():
         f'Hello - testing'
     )
 
-## routes for geojson
-
+## routes for geojson files
 ## crime geojson
 
 ## neighborhood geojson
@@ -48,43 +47,33 @@ def home():
 ## station geojson
 
 ## route for bar chart
-# @app.route('/api/crime-type-bar-chart')
-# def crime_type():
+@app.route('/api/crime-type-bar-chart/<nCode>')
+def crime_type(nCode):
+           
+    nCode = nCode.upper()
     
     # create session from Python to the DB
-    # session = Session(engine)
-    
-    # crime_type = session.query(Crime.crime_type).all()
-    
-    # session.close()
-    
-    # crime_list = [{x.crime_type} for x in crime_type]
-    # return jsonify(crime_list)
-
-## route for bar chart
-@app.route('/api/crime-type-bar-chart')
-def crime_type():
-    
-    # create session from Python to the DB - Seth
     conn = engine.connect()
-    crime_df = pd.read_sql("select * from cobra_merged", conn)
-    crime_json = crime_df.to_json(path_or_buf=None, orient=None, date_format=None, double_precision=10, force_ascii=True, date_unit='ms', default_handler=None, lines=False, compression='infer', index=True, indent=None, storage_options=None)
-    return crime_json
-    # crime_count_df = crime_df['crime_type'].value_counts()
-    # crime_count_dict = crime_count_df(orient="records")
-    # return jsonify(crime_df)
-
     
-    # Alternate method
-    # session = Session(engine)
-    # crime = session.query(Crime.difference_in_lat).all()
-    # crime_list = [{x.difference_in_lat} for x in crime]
-    # print(crime_list)
+    # import data from postgres
+    # use this query to filter data by geoid passed in the route
+    query = "select geoid, crime_type from cobra_merged where geoid = '" + nCode + "'"
     
-    # session.close()
+    # use this query to return all records
+    # query = "select geoid, crime_type from cobra_merged"
     
-    # return jsonify(crime_list)
+    # retrieve data from postgres
+    crime_df = pd.read_sql(query, conn)
     
+    # count number of each crime by neighborhood
+    crime_count = crime_df.groupby('geoid')['crime_type'].value_counts()
+    crime_count = crime_count.unstack()
+    
+    # include this code to pass geoid in json
+    # crime_count = crime_count.reset_index()
+        
+    crime_json = crime_count.to_json(orient='records', index=True)
+    return crime_json    
 
 ## route for other chart
 
