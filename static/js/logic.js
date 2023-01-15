da2cf48f9b10e5921a23523e05bbc7f5069315d4
Meeyoung Park <parkmee@gmail.com>
2023-01-15 10:05:55 -0500

    connected logic to api routes

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,15 @@
 // path to data files
-var crime_path = '/api/crime.geojson'
-var neighborhood_path = 'data/City_of_Atlanta_Neighborhood_Statistical_Areas.geojson'
-var station_path = 'data/Transit_Rail_stations.geojson'
-var neighborhood_stats_path = 'data/Atlanta_Neighborhood_Data_raw.csv'
+// var crime_path = '/api/crime.geojson'
+// var neighborhood_path = 'data/City_of_Atlanta_Neighborhood_Statistical_Areas.geojson'
+// var station_path = 'data/Transit_Rail_stations.geojson'
+// var neighborhood_stats_path = 'data/Atlanta_Neighborhood_Data_raw.csv'
 
 // save data to variables
 var crime_data = d3.json('api/crime.geojson').then(d => d);
-console.log(crime_data)
-var neighborhood_data = d3.json(neighborhood_path).then(d => d);
-var station_data = d3.json(station_path).then(d => d);
+var neighborhood_data = d3.json('/api/neighborhood.geojson').then(d => d);
+var station_data = d3.json('/api/stations.geojson').then(d => d);
 // TODO - create record for overall stats for metro Atlanta (Ryan)
-var neighborhood_stats = d3.csv(neighborhood_stats_path).then(d => d)
+var neighborhood_stats = d3.csv('/api/neighborhood-info/<nCode>').then(d => d)
 
 // initialize webpage
 function init(){
